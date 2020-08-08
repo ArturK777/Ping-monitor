90dd4c0d7aa3b7d39a3ef5bf19ff87f11f8cae01
ArturK777 <ArturK777@users.noreply.github.com>
2020-08-08 08:34:43 +0300

    Update parse.f

--- parse.f
+++ parse.f
@@ -1,4 +1,12 @@
-CREATE tSTR 100 ALLOT \ temp string buffer
-tSTR 100 SOURCE!
-S" 10.0.0.100 temp 5"
-TIB 100 DUMP
+
+: PARSE@ ( c-addr u -- c-addr1 u1 ) 
+    BEGIN 0
+    WHILE
+    REPEAT
+;
+: TEMP  ( -- c-addr u )  S" 10.0.0.100 temp 5" ;
+
+TEMP
+PARSE@
+
+ .S
